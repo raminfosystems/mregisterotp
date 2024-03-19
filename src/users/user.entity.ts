8ad624a19f1e27e49f766e0c9ee0cf066d7382b8
Ravi Lodhiya <ravi@raminfosystems.co.uk>
2024-03-19 14:01:11 +0000

    Refactor UserEntity to User with additional columns

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,1 +1,35 @@
-export class UserEntity {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('Users')
+export class User {
+  constructor(name: string, mobile: string) {
+    this.name = name;
+    this.mobile = mobile;
+  }
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'name', length: 70, nullable: false })
+  name: string;
+
+  @Column({ name: 'mobile', length: 12, nullable: false })
+  mobile: string;
+
+  @Column({ name: 'logged', nullable: true })
+  logged?: boolean;
+
+  @Column({ nullable: true })
+  @CreateDateColumn()
+  created?: string; // date
+
+  @Column({ nullable: true })
+  @UpdateDateColumn()
+  updated?: string; // date
+}
